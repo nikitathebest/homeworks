5d0a283a61381f9174bca7ecf281b1d52f43e54e
nikitathebest <nikita.m.7007@gmail.com>
2019-03-28 04:38:15 +0300

    Fix the equation in task 2

--- HW_02/Nikita_Mishuta/nikita_mishuta_hw_02_t_02.rb
+++ HW_02/Nikita_Mishuta/nikita_mishuta_hw_02_t_02.rb
@@ -1,3 +1,3 @@
 puts 'The equation from 1, 3, 4, 6 should be:'
-equation = 6 / (1 - (3.0 / 4))
-puts "6/(1-(3/4)) in order to get #{equation.to_i}"
+equation = 3 * 4 + 6 * 1 + 6
+puts "(3*4+6*1+6) in order to get #{equation.to_i}"
